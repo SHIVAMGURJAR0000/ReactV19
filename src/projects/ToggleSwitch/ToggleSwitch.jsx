0703fcb2268407742c6583ed8eee44ebbc0ed3ab
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-04-27 03:52:49 +0530

    React Icons

--- src/projects/ToggleSwitch/ToggleSwitch.jsx
+++ src/projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./ToggleSwitch.css";
+import { IoIosSwitch } from "react-icons/io";
 export const ToggleSwitch = () => {
   const [isON, setIsOn] = useState(false);
 
@@ -8,14 +9,24 @@ export const ToggleSwitch = () => {
   };
 
   return (
-    <div
-      className="toggle-switch"
-      onClick={handleTOggleSwitch}
-      style={{ backgroundColor: isON ? "#4caf50" : "#f44336" }}
-    >
-      <div className={`switch ${isON ? "on" : "off"}`}>
-        <span className="switch-state">{isON ? "on" : "Off"}</span>
+    <>
+      <h1 className="center">
+        Toggle Switch{" "}
+        <IoIosSwitch
+          style={{
+            color: "red",
+          }}
+        />
+      </h1>
+      <div
+        className="toggle-switch"
+        onClick={handleTOggleSwitch}
+        style={{ backgroundColor: isON ? "#4caf50" : "#f44336" }}
+      >
+        <div className={`switch ${isON ? "on" : "off"}`}>
+          <span className="switch-state">{isON ? "on" : "Off"}</span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
