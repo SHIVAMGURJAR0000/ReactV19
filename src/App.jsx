8dee8a9bb7752475dc446e12e7ba4c8ea6050a19
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-02 03:11:00 +0530

    short circuit evaluation code write remaining

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,30 @@ import { Keys } from "./components/keys";
 
 export const App = () => {
   return (
-    //keys in react js
+    //Short circuit evalutaion in react js
+    //code writing is remaining
     <section>
       <Keys />
     </section>
   );
 };
+
+// short circuit is a technique used in js where expression are evaluated from left to right
+//logical or
+//expr1 ||expr 2
+// ex: result = false || "hello" // hello
+//  result = true||"world"  //true
+
+//and
+// ex : exp1 = true&&"hello"//hello
+// exp2= fasle&&"world" //false
+
+// NUllish Coalescing(??)
+
+// synatx  expr1??exp2
+
+// result = null??"hello" // hello
+
+// result = undefined??"world" // world
+
+// result = ''??"Fallback" //''
---
+++ src/components/ShortCircuit.jsx
@@ -0,0 +1,3 @@
+export const shortCircuitExample = () => {
+  return <section></section>;
+};
