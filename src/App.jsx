4e68827ecafe5046b6d036702799d4bec2e5a749
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-03 15:32:22 +0530

    React form normal way

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
 import React from "react";
-// import { Counter } from "./components/hooks/useState";
-import { UseEffectChanllenge } from "./components/hooks/useState/challenge";
+import { RegistrationForm } from "./components/hooks/useState/Registration";
 
 export const App = () => {
-  //controlled vs uncontrolled component
-  return <UseEffectChanllenge />;
+  //Registration form
+  return <RegistrationForm />;
 };
-
-//Theory
-
-// 1) let you use different React features from your componenets.
-// 2) useState, useEffects, useContext, useReducer or more.
-// 3) only be used at the top level of your top level compnonent.
-// 4) Do not call hooks inside loops , conditions, or nested functions.
-// 5) Hooks can not be use in regular js and not in class also , only use in fucntion based components.
-// 6) we can create our own custom hooks buy using use+functionName (ex => useFetch, useShivam)
-
-// controlled vs uncontrolled
-
-// what is control and uncontroll
-
-// controlled component in form
-// - where form data is handled by react component state(menas hook) then it is called controlled.
-// - read slides
-// - we will get warning in console when we change controlled to uncontrolled and vice-cersa
-// - controlled componenet -
-//  - when text field use hook like useState
-//  - when we are using event handlers like onChngange, onBlur, etc.
-//  - state updation
-
-// uncontrolled
-// - direct dom manipulation.
-// - event handling
-// - Accessing form data
-
-// React power controll the form
-// when we use direct dom manipulation which is called traditional way then we can't use react powers.
---
+++ src/components/hooks/useState/Registration.jsx
@@ -0,0 +1,166 @@
+//* Registration Form Using Multiple State Variables
+
+//todo  Tasks:
+
+//? Set up a functional component in React.
+//? Create five separate state variables (firstName, lastName, email, password, phoneNumber).
+//? Create input fields for each piece of information.
+//? Implement onChange handlers to update state variables.
+//? Discuss the benefits and drawbacks of this approach.
+
+import "./index.css";
+import { useState } from "react";
+
+export const RegistrationForm = () => {
+  //State Variable
+  // This is Right way but what if we have lot of variable
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  //To get the data
+  const handleInputChange = (e) => {
+    // console.log(e.target.value);
+    console.log(e.target.name);
+    const { name, value } = e.target;
+    // value = e.target.value //already destructure
+
+    switch (name) {
+      case "firstName":
+        setFirstName(value);
+        break;
+
+      case "lastName":
+        setLastName(value);
+        break;
+
+      case "email":
+        setEmail(value);
+        break;
+
+      case "password":
+        setPassword(value);
+        break;
+
+      case "phone":
+        setPhoneNumber(value);
+        break;
+    }
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const formData = {
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+    };
+
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div className="container">
+          <h1>Sign Up</h1>
+          <p>Please fill in this form to create an account.</p>
+
+          <label htmlFor="firstName">
+            <b>First Name</b>
+          </label>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="Enter firstName"
+            required
+            value={firstName} //controlled
+            onChange={handleInputChange}
+          />
+
+          <label htmlFor="lastName">
+            <b>Last Name</b>
+          </label>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Enter lastName"
+            required
+            value={lastName} //controlled
+            onChange={handleInputChange}
+          />
+
+          <label htmlFor="email">
+            <b>Email</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            name="email"
+            required
+            value={email} //controlled
+            onChange={handleInputChange}
+          />
+
+          <label htmlFor="password">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            required
+            value={password} //controlled
+            onChange={handleInputChange}
+          />
+
+          <label htmlFor="phone">
+            <b>Phone Number</b>
+          </label>
+
+          <input
+            type="phone"
+            name="phone"
+            placeholder="9876543211"
+            required
+            value={phoneNumber} //controlled
+            onChange={handleInputChange}
+          />
+
+          <p>
+            By creating an account you agree to our
+            <a href="#" style={{ color: "dodgerblue" }}>
+              Terms & Privacy
+            </a>
+          </p>
+
+          <div className="clearfix">
+            <button type="submit" className="btn">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <section
+        className="summary"
+        style={{ textAlign: "center", marginTop: "30px" }}
+      >
+        <p>
+          Hello, my name is
+          <span>
+            {firstName} {lastName}
+          </span>
+          . My email address is <span>{email}</span> and my phone number is
+          <span>{phoneNumber}</span>.
+        </p>
+      </section>
+    </>
+  );
+};
+
+// If we are using setState for inputfield value then we have to use onChnage function because of controlled state of form
