b6e8b27bc39b54014db0769ef1de9cf506e71717
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-03 14:39:57 +0530

    controlled vs uncontrolled theory

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { UseEffectChanllenge } from "./components/hooks/useState/challenge";
 
 export const App = () => {
-  //UseState hook + useState challenge
+  //controlled vs uncontrolled component
   return <UseEffectChanllenge />;
 };
 
@@ -15,3 +15,24 @@ export const App = () => {
 // 4) Do not call hooks inside loops , conditions, or nested functions.
 // 5) Hooks can not be use in regular js and not in class also , only use in fucntion based components.
 // 6) we can create our own custom hooks buy using use+functionName (ex => useFetch, useShivam)
+
+// controlled vs uncontrolled
+
+// what is control and uncontroll
+
+// controlled component in form
+// - where form data is handled by react component state(menas hook) then it is called controlled.
+// - read slides
+// - we will get warning in console when we change controlled to uncontrolled and vice-cersa
+// - controlled componenet -
+//  - when text field use hook like useState
+//  - when we are using event handlers like onChngange, onBlur, etc.
+//  - state updation
+
+// uncontrolled
+// - direct dom manipulation.
+// - event handling
+// - Accessing form data
+
+// React power controll the form
+// when we use direct dom manipulation which is called traditional way then we can't use react powers.
