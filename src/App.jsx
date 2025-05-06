67bc75dadcde08a1d5d50b1144a57001e4fd1f9d
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-06 07:16:54 +0530

    async await api fetching

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,6 @@ import React from "react";
 import { HowNotToFetchApi } from "./components/hooks/useEffect/HowNotToFetchApi";
 
 export const App = () => {
+  // Fetch api using async and await
   return <HowNotToFetchApi />;
 };
--- src/components/hooks/useEffect/HowNotToFetchApi.jsx
+++ src/components/hooks/useEffect/HowNotToFetchApi.jsx
@@ -6,19 +6,38 @@ export const HowNotToFetchApi = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
+  // const fetchPokemon = () => {
+  //   // fetch("https://jsonplaceholder.typicode.com/posts")
+  //   fetch("http://pokeapi.co/api/v2/pokemon/pikachu") // it will give object not an array
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setpokemon(data);
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       setError(error);
+  //       setLoading(false);
+  //     });
+  // };
+
+  //using async await
+
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch("http://pokeapi.co/api/v2/pokemon/pikachu");
+      const data = await res.json();
+      setpokemon(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    // fetch("https://jsonplaceholder.typicode.com/posts")
-    fetch("http://pokeapi.co/api/v2/pokemon/pikachu") // it will give object not an array
-      .then((res) => res.json())
-      .then((data) => {
-        setpokemon(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setError(error);
-        setLoading(false);
-      });
+    fetchPokemon();
   }, []);
 
   if (loading) {
@@ -59,7 +78,19 @@ export const HowNotToFetchApi = () => {
               className="pokemon-image"
             />
           </figure>
+
           <h1>{pokemon.name}</h1>
+          <div className="grid-three-cols">
+            <p className="pokemon-info">
+              Height: <span> {pokemon.height} </span>
+            </p>
+            <p className="pokemon-info">
+              Weight: <span> {pokemon.weight}</span>
+            </p>
+            <p className="pokemon-info">
+              speed: <span>{pokemon.stats[5].base_stat}</span>
+            </p>
+          </div>
         </li>
       </ul>
     </section>
