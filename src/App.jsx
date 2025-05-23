992d63ad34f64fc74ad186b0f647171e3b4e1b53
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-24 01:11:26 +0530

    PropDrilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { UseId } from "./components/hooks/useId";
+import { ParentComponent } from "./components/PropDrilling";
 
 export const App = () => {
-  //use id hook
-  return <UseId />;
+  // prop drilling
+  return <ParentComponent />;
 };
 
-// generate unique id
-// it does not take any parameter
-// return unique id
-// it is used for accessiblity
-// Note : useId should not be used to genetrate keys in a list.
+// it is a pattern where parent component passes data to child component to deeply nested child component through multiple layer of components.
+// even some of the intermediate components do not need the data.
+// some of the intermediate component do not need the data so to solve this we can use context api.
