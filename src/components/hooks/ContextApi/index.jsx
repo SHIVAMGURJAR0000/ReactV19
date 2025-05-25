a4eb7b80136b9ec2b00f01ac19680cca007e6b7d
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-25 10:25:32 +0530

    ContextApi

--- src/components/hooks/ContextApi/index.jsx
+++ src/components/hooks/ContextApi/index.jsx
@@ -1,12 +1,15 @@
 import { createContext } from "react";
 
 //step1
+//Creating context
 export const BioContext = createContext();
 // *****createContext return a Context Component, not a variable
+// Biocontent is a Context component
 // first letter must be capital
 
 // step2
 // Its like warehouse
+// it will provide the data to all the components which are inside this warehouse.
 export const BioProvider = ({ children }) => {
   const myName = "Shivam";
   const myAge = 49;
