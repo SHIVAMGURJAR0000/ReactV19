ddac0b93dafeb05056884a83bcbec9ba4e485283
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-26 02:04:04 +0530

    rules of useContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BioProvider } from "./components/hooks/ContextApi";
 import { Home } from "./components/hooks/ContextApi/Home";
 
 export const App = () => {
-  // Context Api
+  // useContext rule in react
   return (
     <BioProvider>
       <Home />;
@@ -10,14 +10,5 @@ export const App = () => {
   );
 };
 
-// when we send data to grandgrandParent to grand parent then to parent then to child then for grandParent and parent there is no use of data
-// to solve this we use context Api
-
-// Context API is used to avoid prop drilling.
-
-// what is context api?
-// ans. A way to pass the data through the component tree without having to pass props down manually at every level.
-
-// createContext: creates a Context object.
-// Provider: a component that provides the context value to its children.
-// useContext (consumer) : a hook that allows you to consume a context.
+// Rule
+// 1. only children of the cointext provider can access the data of context.
